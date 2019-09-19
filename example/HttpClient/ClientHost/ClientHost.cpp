7283301cc46a89354b86754c50b30fd52ccaa727
邢俊杰 <67541967@qq.com>
2019-09-19 14:19:49 +0800

    添加http client的example

---
+++ example/HttpClient/ClientHost/ClientHost.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+#include <string>
+#include <mono-bind.hpp>
+
+int main()
+{
+	MonoBind::initDir(
+		MONO_PATH_STR "/lib",
+		MONO_PATH_STR "/etc",
+		MONO_PATH_STR "/etc/mono/4.5/machine.config");
+	MonoBind::Domain::get().initJit("ServerHost.exe", "v4.0.30319");
+	MonoBind::Domain::get().setConfig(MONO_PATH_STR "/etc/mono/4.5", "machine.config");
+
+	auto klass = MonoBind::Domain::get().openAssembly("ClientLib.dll").getImage().classFromName("ClientLib", "Client");
+	auto obj = klass.New();
+	auto content = obj.invoke("Get", "http://www.cplusplus.com/").to<std::string>();
+	// content is utf-8 encoding
+	std::cout << content << std::endl;
+	return 0;
+}
---
+++ example/HttpClient/ClientLib/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientLib
+{
+    public class Client
+    {
+        public string Get(string url)
+        {
+            using(var client = new HttpClient())
+            {
+                return client.GetStringAsync(url).Result;
+            }
+        }
+    }
+}
